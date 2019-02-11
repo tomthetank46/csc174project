bbf981112dd842ec4614bd28d9d03276ecacf168
definitelynotavirus <jdalton9@u.rochester.edu>
2019-02-11 16:41:23 -0500

    Edited php and Readme

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# csc174project
+# Assignment 4
+
+## Domain
+The website focuses on the discussion of different novels about the human condition, high-street fashion and the families that created some major companies, and the history of designer houses and how they have changed over time to meet people's needs.
+
+## Ontology
+### Overall Website
+- Each webpage has a focus on things related to people 
+### Index
+- Each webpage has a list of topics
+- Each webpage has a creative description
+### Novel
+- Each novel has an introduction list
+- Each novel has a plot summary
+- Each novel has a writing background
+- Each novel has adaptations
+### High-Street Fashion
+- Each company has an introduction
+- Each company has a brief history
+- Each company has features
+### Designer House
+- Each designer house has a brief history
+- Each designer house has a creative director
+- Each designer house has a 21st Century discussion
+
+## Taxonomy 
+### Index
+- Top category is about the webpage as a whole
+-- h1 tag for the title
+-- nav bar for navigation
+-- footer
+
+-- Second category is of the topics
+--- H2 tag is the name of a webpage
+--- List of subtopics
+--- Description for topic
+
+### Novel
+- Top category is about the webpage as a whole
+-- h1 tag for the title
+-- nav bar for navigation
+-- footer
+
+-- Second category is of the novels
+--- List of general introduction
+--- Plot summary
+--- Writing background
+--- Adaptations
+
+### High-street fashion
+- Top category is about the webpage as a whole
+-- h1 tag for the title
+-- nav bar for navigation
+-- footer
+
+-- Second category is of the companies
+--- Introduction
+--- Brief History
+--- List of Features
+
+### Designer House
+- Top category is about the webpage as a whole
+-- h1 tag for the title
+-- nav bar for navigation
+-- footer
+
+-- Second category is of the Designer houses
+--- Brief history
+--- List of Creative Directors
+--- 21st Century
+
+## Choreography
+1. Homepage (index page)
+2. Novel
+2. High-street fashion
+2. Designer house
--- lab03/index.php
+++ lab03/index.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-
-<html lang="en">
-
-<head>
-	<meta charset="utf-8">
-	<title>When Novel Meets Fashion | Lab 3: Smash Website | Team Hong Kong</title>
-	<link rel="stylesheet" href="css/home.css">
-	<link rel="stylesheet" href="css/navigation.css">
-	<link rel="stylesheet" href="css/footer.css">
-</head>
-
-<body class="container">
-
+<?php $currentPage="When Novel Meets Fashion| Lab 3: Smash Website | Team Hong Kong";
+include "inc/html-top.inc";?>
 
 	<header>
-			<h1>When Novel Meets Fashion</h1>
+		<h1>When Novel Meets Fashion</h1>
 	</header>
 
 	<?php include "inc/nav.inc"; ?>
@@ -23,40 +11,63 @@
 	<main>
 
 		<section class="excerpt">
-		<div class="text">
-		<h2>Novel</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<a href="novel.php">Read More</a>
-		</div>
-		<img src="images/novel.jpg"
-		     alt="a picture about novel">
+			<div class="text">
+				<h2>Novel</h2>
+
+				<ul>
+					<li>"The Sun Also Rises" (1920)</li>
+					<li>"A Farewell to Arms" (1929)</li>
+					<li>"For Whom the Bell Tolls" (1940)</li>
+				</ul>
+
+				<p>These novels written in the 20th century offer a unique insight into the time periods in which they were written, and in turn allow us to peek into the human condition.</p>
+
+				<a href="novel.php">Read More</a>
+			</div>
+			<img src="images/novel.jpg"
+		    	alt="a picture about novel">
 		</section>
 
 		<section class="excerpt">
-		<div class="text">
-		<h2>High-Street Fashion</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<a href="sfashion.php">Read More</a>
-		</div>
-		<img src="images/sfashion.jpg"
-		     alt="a picture about high street fashion">
+			<div class="text">
+
+				<h2>High-Street Fashion</h2>
+
+				<ul>
+					<li>Everlane</li>
+					<li>Forever 21</li>
+					<li>American Eagles</li>
+				</ul>
+
+				<p>Fame. Wealth. Fashion. Once unattainable by any except for the social elite, these characteristics were envied by all. However, with the development of high-street fashion, these companies with humble, family-owned beginnings now allow people to experience fashion in a way never before seen.</p>
+
+				<a href="sfashion.php">Read More</a>
+			</div>
+			<img src="images/sfashion.jpg"
+			     alt="a picture about high street fashion">
 		</section>
 
 		<section class="excerpt">
-		<div class="text">
-		<h2>Designer House</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<a href="designerhouse.php">Read More</a>
-		</div>
-		<img src="images/dhouse.jpg"
-		     alt="a picture about designer house">
+			<div class="text">
+				<h2>Designer House</h2>
+
+				<ul>
+					<li>Louis Vuitton</li>
+					<li>Chanel</li>
+					<li>Dior</li>
+				</ul>
+
+				<p>Fashion isn't created over night, and neither are the companies that make it. Designer houses have a unique history of innovation and inspiration representative of humans ingenuity and creativity. They play a big role in the creation of style and in tern help shape cultures.</p>
+				
+				<a href="designerhouse.php">Read More</a>
+			</div>
+			<img src="images/dhouse.jpg"
+			     alt="a picture about designer house">
 		</section>
 		
 	</main>
 
-	<footer> © CSC 174: Advanced Front-end Web Design and Development</footer>
-
-	
+	<footer> © CSC 174: Advanced Front-end Web Design and Development</footer>	
 
     <?php include "inc/scripts.inc"; ?>
 
