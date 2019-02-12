fd665a93242fc69b949073f88464434b7413b9db
definitelynotavirus <jdalton9@u.rochester.edu>
2019-02-12 12:19:57 -0500

    Update index.php

--- lab03/index.php
+++ lab03/index.php
@@ -20,12 +20,12 @@ include "inc/html-top.inc";?>
 					<li>"For Whom the Bell Tolls" (1940)</li>
 				</ul>
 
-				<p>These novels written in the 20th century offer a unique insight into the time periods in which they were written, and in turn allow us to peek into the human condition.</p>
+				<p>These novels written in the 20th century offer a unique insight into the time periods in which they were written, and in turn allow us to peek into the human condition. "The Sun Also Rises" is a love story between a man whose war wound has made him unable to have sex and a promiscuous divorcée. "A Farewell to Arms" is about an American paramedic serving in the Italian Army and his journey through the war and towards love. "For Whom the Bell Tolls" is about an American who had lived in Spain during the pre-war period who is drawn into the war in unexpected ways.</p>
 
 				<a href="novel.php">Read More</a>
 			</div>
 			<img src="images/novel.jpg"
-		    	alt="a picture about novel">
+		    	 alt="a picture about novel">
 		</section>
 
 		<section class="excerpt">
@@ -39,7 +39,7 @@ include "inc/html-top.inc";?>
 					<li>American Eagles</li>
 				</ul>
 
-				<p>Fame. Wealth. Fashion. Once unattainable by any except for the social elite, these characteristics were envied by all. However, with the development of high-street fashion, these companies with humble, family-owned beginnings now allow people to experience fashion in a way never before seen.</p>
+				<p>Fame. Wealth. Fashion. Once unattainable by any except for the social elite, these characteristics were envied by all. However, with the development of high-street fashion, these companies with humble, family-owned beginnings now allow people to experience fashion in a way never before seen. Everlane is a United States clothing retailer that sells primarily online. The organization is headquartered in San Francisco, California and also has a store in New York City. Forever 21 is an American fast fashion retailer headquartered in Los Angeles, California. American Eagle Outfitters, Inc. is an American lifestyle clothing and accessories retailer.</p>
 
 				<a href="sfashion.php">Read More</a>
 			</div>
@@ -57,7 +57,7 @@ include "inc/html-top.inc";?>
 					<li>Dior</li>
 				</ul>
 
-				<p>Fashion isn't created over night, and neither are the companies that make it. Designer houses have a unique history of innovation and inspiration representative of humans ingenuity and creativity. They play a big role in the creation of style and in tern help shape cultures.</p>
+				<p>Fashion isn't created over night, and neither are the companies that make it. Designer houses have a unique history of innovation and inspiration representative of humans ingenuity and creativity. They play a big role in the creation of style and in tern help shape cultures. The Louis Vuitton label was founded by Vuitton in 1854 on Rue Neuve des Capucines in Paris, France. The House of Chanel (Chanel S.A.) originated in 1909 when Gabrielle Chanel opened a millinery shop at 160 Boulevard Malesherbes. The House of Dior was established on 16 December 1946 in "a private house"[5] at 30 Avenue Montaigne Paris B.</p>
 				
 				<a href="designerhouse.php">Read More</a>
 			</div>
